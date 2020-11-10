494aea4ed3f13f189efb7f5a395a044a8a9d4e9c
Arisa <arisa8533@gmail.com>
2020-11-10 14:22:55 +0700

    person

---
+++ src/data/ToyData.java
@@ -0,0 +1,60 @@
+package data;
+
+import base.Bike;
+import base.Parcel;
+import base.Person;
+
+public class ToyData {
+
+    public static Bike[] getBikes() {
+        Bike[] bikes = new Bike[3];
+        // your code
+
+        return bikes;
+    }
+
+    public static Person[] getPerson() {
+        Person[] person = new Person[3];
+        person[0] = new Person ( ("Nutchanon") , ("Assawachin"));
+        person[1] = new Person ( ("Nutwara") , ("Boonyangkongkaew"));
+        person[2] = new Person ( ("Anuwat") , ("Pattanajan"));
+        
+        return person;
+    }
+
+    public static Parcel[] getParcels() {
+        Parcel[] parcels = new Parcel[10];
+        // your code
+
+        return parcels;
+    }
+
+    //testing
+    public static void main(String[] args) {
+        printBike();
+        printPerson();
+        printParcels();
+    }
+
+    private static void printBike() {
+        Bike[] bikes = getBikes();
+        for (int i = 0; i < bikes.length; i++) {
+            System.out.println(bikes[i].toString());
+        }
+    }
+
+    private static void printPerson() {
+        Person[] person = getPerson();
+        for (int i = 0; i < person.length; i++) {
+            System.out.println(person[i].toString());
+        }
+    }
+
+    private static void printParcels() {
+        Parcel[] percels = getParcels();
+        for (int i = 0; i < percels.length; i++) {
+            System.out.println(percels[i].toString());
+        }
+    }
+
+}
